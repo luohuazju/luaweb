22987124231147a0cbb79ff981c6593074f49882
Carl Luo <cluo@jobs2careers.com>
2017-05-18 16:54:08 -0500

    simple mvc

--- lua/mvc.lua
+++
@@ -1,7 +0,0 @@
-local uri = ngx.var.uri
--- 默认首页
-if uri == "" or uri == "/" then
-    local res = ngx.location.capture("/index.html", {})
-    ngx.say(res.body)
-    return
-end
---
+++ lualib/lite/mvc.lua
@@ -0,0 +1,58 @@
+local uri = ngx.var.uri
+-- home page
+if uri == "" or uri == "/" then
+    local res = ngx.location.capture("/index.html", {})
+    ngx.say(res.body)
+    return
+end
+
+local m, err = ngx.re.match(uri, "([a-zA-Z0-9-]+)/*([a-zA-Z0-9-]+)*")
+
+local is_debug = true      
+
+local moduleName = m[1]     -- module
+local method = m[2]         -- method
+
+if not method then
+    method = "index"        -- default method index
+else
+    method = ngx.re.gsub(method, "-", "_")    
+end
+
+-- controller is under web
+local prefix = "web."       
+local path = prefix .. moduleName
+
+-- input the module web.user
+local ret, ctrl, err = pcall(require, path)
+
+if ret == false then
+    if is_debug then
+        ngx.status = 404
+        ngx.say("<p style='font-size: 50px'>Error: <span style='color:red'>" .. ctrl .. "</span> module not found !</p>")
+    end
+    ngx.exit(404)
+end
+
+-- method of the module, get, index
+local req_method = ctrl[method]
+
+if req_method == nil then
+    if is_debug then
+        ngx.status = 404
+        ngx.say("<p style='font-size: 50px'>Error: <span style='color:red'>" .. method .. "()</span> method not found in <span style='color:red'>" .. moduleName .. "</span> lua module !</p>")
+    end
+    ngx.exit(404)
+end
+
+-- call lua method
+ret, err = pcall(req_method)
+
+if ret == false then
+    if is_debug then
+        ngx.status = 404
+        ngx.say("<p style='font-size: 50px'>Error: <span style='color:red'>" .. err .. "</span></p>")
+    else
+        ngx.exit(500)
+    end
+end
